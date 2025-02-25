544aa59a70b1d9be16cd4c5a3788806592f24012
Pablo Ferrari <pablo@Asgard.local>
2025-02-24 20:21:38 -0800

    hygiene

--- client/desktop-app/react/src/App.tsx
+++ client/desktop-app/react/src/App.tsx
@@ -1,15 +1,14 @@
-import usePageRouter from "./hooks/usePageRouter.tsx";
-
+import usePageRouter  from "./hooks/usePageRouter.tsx";
+import ScanPage       from "./pages/scan/scan.tsx";
+import SearchPage     from "./pages/search/search.tsx";
 import "./styles/App.css";
-import ScanPage from "./pages/scan/scan.tsx";
-import SearchPage from "./pages/search/search.tsx";
 
 function App() {
 
-  const pageUri = usePageRouter();
+  const currentPageUri = usePageRouter();
 
   const Page = () => {
-    switch(pageUri) {
+    switch(currentPageUri) {
       case "/":
         return <ScanPage/>;
       case "/scan":
